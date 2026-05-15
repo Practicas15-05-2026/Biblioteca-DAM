3640d7b80cb87be4392066216f8e2b5b4ab643fc
miguel <a28715@svalero.com>
2026-05-15 19:48:27 +0200

    se pueden subir imágenes al registrar libros

--- Backend/app.js
+++ Backend/app.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '..', 'Frontend')));
 app.use('/css', express.static(path.join(__dirname, '..', 'css')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api', rutas);
 app.listen(puerto, () => {
     console.log(`Servidor corriendo en: http://localhost:${puerto}`);
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ruta = express.Router();
 const controller = require('./controller');
+const subirImagen = require('./image');
 
 ruta.get('/usuarios', controller.obtenerUsuarios);
 ruta.post('/usuarios', controller.crearUsuarios);
@@ -8,8 +9,8 @@ ruta.put('/usuarios/:id', controller.actualizarUsuario);
 ruta.delete('/usuarios/:id', controller.borrarUsuario);
 
 ruta.get('/libros', controller.obtenerLibros);
-ruta.post('/libros', controller.crearLibros);
-ruta.put('/libros/:id', controller.actualizarLibros);
+ruta.post('/libros', subirImagen.single('imagen'), controller.crearLibros);
+ruta.put('/libros/:id', subirImagen.single('imagen'), controller.actualizarLibros);
 ruta.delete('/libros/:id', controller.borrarLibros);
 
 module.exports = ruta;
